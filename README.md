845bf432a7860ad64b75af36eb055cb8f86cc2e0
Aaron Olkin <aaronolkin@gmail.com>
2015-07-28 15:10:28 -0400

    Added global hotkeys for transport controls

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Spotify Remote
 A Google Chrome browser action and panel extension for controlling the Spotify Web Player.
 
 It adds a browser action popup with transport buttons, album art, and track information, that can also be opened as a panel (with the appropriate flag enabled).
+
+GPM steals media keys, so I instead globally registered Ctrl+Shift+7 as previous track, Ctrl+Shift+8 as play/pause, and Ctrl+Shift+9 as next track. On Mac, use
+Cmd instead of Ctrl.
--- js/bg.js
+++ js/bg.js
@@ -10,3 +10,25 @@ function processData(data,sender,sendResponse){
 }
 
 chrome.runtime.onMessage.addListener(processData);
+
+
+function send(message,callback) {
+    if (!callback) {
+	callback = processData;
+    }
+    chrome.tabs.query({url: "https://play.spotify.com/*"}, function(tabs) {
+	if (tabs.length < 1) {
+	    chrome.tabs.create({
+		url: "https://play.spotify.com/",
+		pinned: true
+	    });
+	} else {
+	    chrome.tabs.sendMessage(tabs[0].id, message, callback);
+	}
+    });
+}
+
+chrome.commands.onCommand.addListener(function(command){
+    console.log(command);
+    send({action: command});
+});
